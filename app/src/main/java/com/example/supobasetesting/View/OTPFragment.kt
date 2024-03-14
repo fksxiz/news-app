e715ffd69ebc1b6c47b35028fd556aa9b174ce0c
fksxiz <malyshkin_04@bk.ru>
2024-03-14 19:16:49 +0300

    added timer

--- app/src/main/java/com/example/supobasetesting/View/OTPFragment.kt
+++ app/src/main/java/com/example/supobasetesting/View/OTPFragment.kt
@@ -2,12 +2,14 @@ package com.example.supobasetesting.View
 
 import android.content.Context
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.TextView
 import com.chaos.view.PinView
 import com.example.supobasetesting.R
 
@@ -15,6 +17,9 @@ import com.example.supobasetesting.R
 class OTPFragment : Fragment() {
 
     private lateinit var pinView:PinView
+    private lateinit var timeTextView:TextView
+
+    private var time:Int=60
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +40,23 @@ class OTPFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         view.apply {
             pinView = findViewById(R.id.otpPinView)
+            timeTextView = findViewById(R.id.timerTextView)
         }
+        startTimer()
     }
 
+    fun startTimer(){
+        time=60
+        val timer = object :CountDownTimer(60000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                time--
+                timeTextView.setText(time.toString())
+            }
+
+            override fun onFinish() {
+            }
+        }.start()
+    }
 
     companion object {
 
